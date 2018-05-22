5fe05737f5504a650e9b700676592fdd41cbd4b9
Sose1 <bazixx123@gmail.com>
2018-05-22 18:56:28 +0200

    A takie notatki ...

---
+++ app/src/main/java/com/example/sose1/plumnotes/Note.java
@@ -0,0 +1,14 @@
+package com.example.sose1.plumnotes;
+
+public class Note {
+
+    private int ID;
+    private String title;
+    private String content;
+
+    public Note(int ID, String title, String content) {
+        this.ID = ID;
+        this.title = title;
+        this.content = content;
+    }
+}
